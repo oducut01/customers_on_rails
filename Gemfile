5cff5c88bffce1d5f880818aeb9146bb306e34f2
Omar Ducut <oducut@academic.rrc.ca>
2021-03-21 18:51:18 -0500

    Adds Customer Partial

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'jbuilder', '~> 2.7'
 
 gem 'activeadmin'
 gem 'devise'
+gem 'image_processing'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :customers, only: %i[index show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "customers#index"
 end
