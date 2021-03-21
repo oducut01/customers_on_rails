a8bec0a211b6ea150919c2b35506f811b8fe2a36
Omar Ducut <oducut@academic.rrc.ca>
2021-03-21 17:13:41 -0500

    Added image to Customer Model and image upload

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -4,7 +4,17 @@ ActiveAdmin.register Customer do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :phone, :email, :notes
+  permit_params :name, :phone, :email, :notes, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
+
   #
   # or
   #
